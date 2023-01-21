9a751745a9c2e306d4504d542bce2a9f189c1eb2
Landon Dolvin <landond@mit.edu>
2023-01-20 20:26:35 -0500

    added new created groups files

---
+++ client/src/components/modules/CreatedGroupPic.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./GroupPic.css";
+import { Link } from "@reach/router";
+
+export const GroupPic = (props) => {
+  return (
+    <div className="GroupPic-pic">
+      <img src={props.img_url} className="GroupPic-image"></img>
+    </div>
+  );
+};
