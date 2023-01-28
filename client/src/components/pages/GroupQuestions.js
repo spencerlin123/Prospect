54aa886642f52373aa2e7cbf6ce359e3888d78d1
Landon Dolvin <landond@mit.edu>
2023-01-27 19:45:38 -0500

    fixed css for spencers page

--- client/src/components/pages/GroupQuestions.js
+++ client/src/components/pages/GroupQuestions.js
@@ -6,7 +6,9 @@ import DisplayQuestions from "../modules/DisplayQuestions";
 function GroupQuestionsPage(props) {
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
-    get("/api/group-questions", {group_code: props.code}).then((questions) => {
+    get("/api/group-questions", {
+      group_code: props.code,
+    }).then((questions) => {
       setQuestions(questions);
       console.log(props.code);
       console.log(questions);
@@ -14,13 +16,25 @@ function GroupQuestionsPage(props) {
     });
   }, []);
   return (
-    <div className="GroupQuestions-page">
-      {questions.map((question) => (
-        <div>
-          <DisplayQuestions question={question} code={props.code} googleid={props.googleid} />
+    <>
+      <div className="GroupQuestions-emptyspace" />
+      <div className="GroupQuestions-page">
+        <div className="grayinfo-container">{props.title}</div>
+        <div className="Questions-container">
+          <div className="GroupQuestions-header">QUESTIONS</div>
+          <div className="GroupQuestions-body">
+            {questions.map((question) => (
+              <div>
+                <DisplayQuestions question={question} code={props.code} googleid={props.googleid} />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </div>
+      </div>
+      <div className="Questions-line">
+        <button className="Questions-submit">SUBMIT</button>
+      </div>
+    </>
   );
 }
 
